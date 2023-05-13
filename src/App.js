c13438cca5968e1c8df9cf223cbb8d77c42a8f53
Noa Trachez <74274767+Noa-Trachez@users.noreply.github.com>
2023-05-13 20:58:34 +0200

    Mdr

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         En production...
+         Snapchat : nono.trachez
         </p>
       </header>
     </div>
